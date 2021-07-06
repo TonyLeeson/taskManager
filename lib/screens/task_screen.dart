e217ea2531b2ae5c333073b6c1d7b104361b6e9b
TonyLeeson <49373713+TonyLeeson@users.noreply.github.com>
2021-07-06 14:11:57 +0100

    Added: database

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/db/tasks_database.dart';
 import 'package:task_manager/screens/task_entry_screen.dart';
 import 'package:task_manager/utilities/task.dart';
 import 'package:task_manager/utilities/tasks.dart';
 import 'package:task_manager/widgets/task_card.dart';
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TasksScreen extends StatefulWidget {
   // const TasksScreen({Key? key}) : super(key: key);
@@ -13,6 +17,23 @@ class TasksScreen extends StatefulWidget {
 
 class _TasksScreenState extends State<TasksScreen> {
   bool tasksSorted = true;
+  late List<Task> listOfTasks;
+
+  @override
+  void initState() {
+    super.initState();
+    // refreshTasks();
+  }
+
+  @override
+  void dispose() {
+    TasksDatabase.instance.close();
+    super.dispose();
+  }
+
+  refreshTasks() async {
+    this.listOfTasks = await TasksDatabase.instance.readAllTasks();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +41,9 @@ class _TasksScreenState extends State<TasksScreen> {
       tasks
           .sort((a, b) => a.taskDueDateAndTime.compareTo(b.taskDueDateAndTime));
     }
+    refreshTasks();
+    print('Tasks length = ${listOfTasks.length}');
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Task Manager'),
@@ -32,28 +56,33 @@ class _TasksScreenState extends State<TasksScreen> {
       ),
       body: SafeArea(
         // minimum: EdgeInsets.symmetric(horizontal: 10.0),
-        child: ListView.builder(
-          itemCount: tasks.length,
-          itemBuilder: (BuildContext context, int index) {
-            return TaskCard(
-              cardTaskDescription: tasks[index].description(),
-              cardDateAndTimeString: tasks[index].dueDateAndTime(),
-              cardStatusColor: tasks[index].statusColor(),
-              isCompleted: tasks[index].isCompleted,
-              completeOnPress: () {
-                setState(() {
-                  tasks[index].isCompleted
-                      ? tasks[index].isCompleted = false
-                      : tasks[index].isCompleted = true;
-                });
-              },
-              deleteOnPress: () {
-                setState(() {
-                  tasks.removeAt(index);
-                });
-              },
-            );
-          },
+
+        child: Container(
+          height: MediaQuery.of(context).size.height * .7,
+          width: double.infinity,
+          child: ListView.builder(
+            itemCount: listOfTasks.length,
+            itemBuilder: (BuildContext context, int index) {
+              return TaskCard(
+                cardTaskDescription: listOfTasks[index].description(),
+                cardDateAndTimeString: listOfTasks[index].dueDateAndTime(),
+                cardStatusColor: listOfTasks[index].statusColor(),
+                isCompleted: listOfTasks[index].isCompleted,
+                completeOnPress: () {
+                  setState(() {
+                    listOfTasks[index].isCompleted
+                        ? listOfTasks[index].isCompleted = false
+                        : listOfTasks[index].isCompleted = true;
+                  });
+                },
+                deleteOnPress: () {
+                  setState(() {
+                    listOfTasks.removeAt(index);
+                  });
+                },
+              );
+            },
+          ),
         ),
       ),
     );
@@ -69,7 +98,11 @@ class _TasksScreenState extends State<TasksScreen> {
     if (returnedTask != null) {
       print('Returned task was not null');
       setState(() {
-        tasks.add(returnedTask);
+        // tasks.add(returnedTask);
+        TasksDatabase.instance.create(returnedTask);
+        refreshTasks();
+        // print('This is a test print.');
+        // print(TasksDatabase.instance.readAllTasks().toString());
       });
     } else {
       print('returned task was null');
--- lib/utilities/task.dart
+++ lib/utilities/task.dart
@@ -1,15 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class Task {
-  bool isCompleted;
-  String taskText;
-  DateTime taskDueDateAndTime;
+final String tableTasks = 'tasks';
 
-  Task(
-      {required this.taskText,
-      required this.taskDueDateAndTime,
-      required this.isCompleted});
+class TasksFields {
+  static final List<String> values = [
+    id,
+    isComplete,
+    taskText,
+    taskDueDateAndTime
+  ];
+  static final String id = '_id';
+  static final String isComplete = 'isComplete';
+  static final String taskText = 'taskText';
+  static final String taskDueDateAndTime = 'taskDueDateAndTime';
+}
+
+class Task {
+  final int? id;
+  bool isCompleted;
+  final String taskText;
+  final DateTime taskDueDateAndTime;
+
+  Task({
+    this.id,
+    required this.taskText,
+    required this.taskDueDateAndTime,
+    required this.isCompleted,
+  });
+
+  Task copy({
+    int? id,
+    bool? isCompleted,
+    String? taskText,
+    DateTime? taskDueDateAndTime,
+  }) =>
+      Task(
+        id: id ?? this.id,
+        taskText: taskText ?? this.taskText,
+        taskDueDateAndTime: taskDueDateAndTime ?? this.taskDueDateAndTime,
+        isCompleted: isCompleted ?? this.isCompleted,
+      );
+
+  static Task fromJson(Map<String, Object?> json) => Task(
+        id: json[TasksFields.id] as int?,
+        taskText: json[TasksFields.taskText] as String,
+        taskDueDateAndTime:
+            DateTime.parse(json[TasksFields.taskDueDateAndTime] as String),
+        isCompleted: json[TasksFields.isComplete] == 1,
+      );
+
+  Map<String, Object?> toJson() => {
+        TasksFields.id: id,
+        TasksFields.taskText: taskText,
+        TasksFields.taskDueDateAndTime: taskDueDateAndTime.toIso8601String(),
+        TasksFields.isComplete: isCompleted ? 1 : 0,
+      };
 
   Color statusColor() {
     // Returns a Color class with the color depending on the status of the task.
